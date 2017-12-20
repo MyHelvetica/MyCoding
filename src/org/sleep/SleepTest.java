7d169ee7dc62b9a67c40e223494ebd9617976bd5
阿姐的阿杰 <24563287+MyHelvetica@users.noreply.github.com>
2017-12-20 16:34:47 +0800

    增加 sleep

---
+++ src/org/sleep/SleepTest.java
@@ -0,0 +1,43 @@
+package org.sleep;
+
+/**
+ * 多个线程随机调用
+ * @author Helvetica
+ *
+ */
+public class SleepTest implements Runnable{
+
+	public static void main(String[] args) {
+		
+		SleepTest rTest = new SleepTest();
+		/* 注：不要显示创建线程，请使用线程池。 */
+		Thread aThread = new Thread(rTest);
+
+		/* 尽量不要使用魔法值 */
+		for (int index = 0; index < 100; index++) {
+			
+			System.out.println(Thread.currentThread().getName() + " " + index);
+			
+			if (30 == index) {
+				aThread.start();
+				try {
+					// 主线程暂停，立刻执行 aThread
+					Thread.sleep(1);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	@Override
+	public void run() {
+		/* 尽量不要使用魔法值 */
+		for (int index = 0; index < 30; index++) {
+			System.out.println("当前线程 " 
+		+ Thread.currentThread().getName() 
+		+ "：" 
+		+ index);
+		}
+	}
+}
