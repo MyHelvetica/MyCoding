c3689ea863a4de432f241331d26cb9a2e49945fa
阿姐的阿杰 <24563287+MyHelvetica@users.noreply.github.com>
2017-12-20 16:23:19 +0800

    增加 join 测试

---
+++ src/org/join/JoinTest.java
@@ -0,0 +1,42 @@
+package org.join;
+
+/**
+ * 多个线程随机调用
+ * @author Helvetica
+ *
+ */
+public class JoinTest implements Runnable{
+
+	public static void main(String[] args) {
+		
+		JoinTest rTest = new JoinTest();
+		/* 注：不要显示创建线程，请使用线程池。 */
+		Thread aThread = new Thread(rTest);
+
+		/* 尽量不要使用魔法值 */
+		for (int index = 0; index < 100; index++) {
+			
+			System.out.println(Thread.currentThread().getName() + " " + index);
+			
+			if (30 == index) {
+				aThread.start();
+				try {
+					aThread.join();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	@Override
+	public void run() {
+		/* 尽量不要使用魔法值 */
+		for (int index = 0; index < 30; index++) {
+			System.out.println("当前线程 " 
+		+ Thread.currentThread().getName() 
+		+ "：" 
+		+ index);
+		}
+	}
+}
